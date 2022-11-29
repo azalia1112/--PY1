b12ceedcdf0091494d5a115f6e01ba3f85e85152
azalia1112 <115169432+azalia1112@users.noreply.github.com>
2022-11-29 19:55:54 +0300

    Add files via upload

---
+++ Лабораторная работа 6/task 4/task 4.py
@@ -0,0 +1,36 @@
+import json
+
+INPUT_FILE = 'input.csv'
+OUTPUT_FILE = 'output.json'
+
+indent = 4 * ' '
+
+
+def csv_to_list_dict(input_file, new_line='\n', delimiter=',') -> list[dict]:
+    with open(input_file, 'r', encoding='utf-8') as file:
+        header = file.readline().replace(new_line, '').split(delimiter)
+        for line in file:
+            line = line.replace(new_line, '')
+            yield dict(zip(header, line.split(delimiter)))
+
+
+def converter_csv_to_json(input_file, output_file, new_line='\n', delimiter=','):
+    with open(output_file, 'w', encoding='utf-8') as serial:
+        serial.write('[' + new_line)
+        is_first_row = True
+        for record in csv_to_list_dict(input_file):
+            if not is_first_row:
+                serial.write(delimiter + new_line)
+            serial.write(indent + '{' + new_line)
+            is_first_row = True
+            for key, value in record.items():
+                if not is_first_row:
+                    serial.write(delimiter + new_line)
+                serial.write(f'{indent}{indent}"{key}": {value}')
+                is_first_row = False
+            serial.write(new_line + indent + '}')
+            is_first_row = False
+        serial.write(new_line + ']')
+
+
+print(json.dumps(converter_csv_to_json(INPUT_FILE, OUTPUT_FILE), indent=4))
